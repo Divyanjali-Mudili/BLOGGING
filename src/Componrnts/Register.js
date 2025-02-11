39b112b3ec130612459639bd1d95517eb711bddc
workdotnisha <workdotnisha@gmail.com>
2025-02-11 13:09:09 +0530

    Modify login-register page

--- src/Componrnts/Register.js
+++ src/Componrnts/Register.js
@@ -1,6 +1,5 @@
 import {useState} from 'react';
 
-
 export default function Register(){
     const [userName , setuserName ]=useState('');
     const [userEmail, setuserEmail]=useState('');
@@ -15,11 +14,15 @@ export default function Register(){
     return(
         <>
         <form className="register" onSubmit={Register}>
-            <h1>Register</h1>
-            <input type="text" placeholder="userName" value={userName}
-            onchange={ev => setuserName(ev.target.value)}required></input>
-            <input type="email" placeholder="userEmail" value={userEmail}required></input>
-            <button class="btn">Register</button>
+            <h3><span>Sign Up</span> For Free</h3>
+            <p>Let's sign up quickly to get started</p>
+            <input type="text" placeholder="Enter Your Name" value={userName}
+            onChange={ev => setuserName(ev.target.value)}required></input>
+            <input type="email" placeholder="Enter Your Email" value={userEmail} onChange={ev => setuserEmail(ev.target.value)}required></input>
+            <input type="password" placeholder="Enter Your Password" required></input>
+            <input type="password" placeholder="Confirm Your Password" required></input>
+            <button className="btn">Sign Up</button>
+            <p className="redirect-text">Already have an account? <a href="/login">Login</a></p>
         </form>
         </>
     )
